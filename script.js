4183302c719d65e6cb625f266d2878687d89be7a
Trithereon <100091007+Trithereon@users.noreply.github.com>
2025-02-21 20:46:12 -0500

    Generate 16x16 boxes within the container div

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function gridCreate(){
-    const newGridItem = document.createElement("div");
-    newGridItem.classList.add("grid-item");
+
     const gridContainer = document.getElementById("grid-container");
-    gridContainer.appendChild(newGridItem);
+    
+    
+    for (let i = 1; i <= 16; i++){
+        for (let i = 1; i <= 16; i++){
+            const newGridItem = document.createElement("div");
+            newGridItem.classList.add("grid-item");
+            gridContainer.appendChild(newGridItem);
+        }
+        
+        /*const lineBreak = document.createElement('br');
+        gridContainer.appendChild(lineBreak);*/
+    }
 
 }
 
