54798b8837165e399e6fad62c38e4ee01532a93c
Trithereon <100091007+Trithereon@users.noreply.github.com>
2025-02-21 20:23:57 -0500

    Generate a single box within the container div

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function gridCreate(){
+    const newGridItem = document.createElement("div");
+    newGridItem.classList.add("grid-item");
+    const gridContainer = document.getElementById("grid-container");
+    gridContainer.appendChild(newGridItem);
 
+}
 
-
+gridCreate();
