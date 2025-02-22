b4d762a7a6d0fad7d704387632ab219acad03960
Trithereon <100091007+Trithereon@users.noreply.github.com>
2025-02-21 23:53:42 -0500

    Add button which deletes old grid and creates new grid

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function gridCreate(){
+//Default size is 16 squares.
 
-    const gridContainer = document.getElementById("grid-container");
+let numSquares = 16;
+gridCreate(numSquares);
+
+function gridCreate(numSquares){
+
+    let gridContainer = document.getElementById("grid-container");
     
-    for (let i = 1; i <= 256; i++){
-        const newGridItem = document.createElement("div");
-        newGridItem.classList.add("grid-item");
-        gridContainer.appendChild(newGridItem);
+    if (gridContainer === null){
+        const newGridContainer = document.createElement("div");
+        newGridContainer.setAttribute('id', 'grid-container');
+        document.body.appendChild(newGridContainer);
+        gridContainer = document.getElementById("grid-container");
     }
 
+    for (let i = 1; i <= numSquares; i++){
+        for (let i = 1; i <= numSquares; i++){
+            const newGridItem = document.createElement("div");
+            newGridItem.classList.add("grid-item");
+            gridContainer.appendChild(newGridItem);
+        }
+    }
     const gridItems = document.getElementsByClassName("grid-item");
     
     for (let i = 0; i < gridItems.length; i++){
@@ -15,7 +28,13 @@ function gridCreate(){
                 gridItems[i].style.backgroundColor = 'black';
         });
     }
+    
 
 }
 
-gridCreate();
+numSquares = gridCreateButton.addEventListener("click", function(){
+    numSquares = prompt("How many boxes per side do you want for the next grid? (1-100)");
+    let grid = document.getElementById("grid-container");
+    grid.remove();
+    gridCreate(numSquares);
+});
