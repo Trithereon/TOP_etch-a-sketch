067a03528890ed6ccfd4bec456dd2247ee911bfe
Trithereon <100091007+Trithereon@users.noreply.github.com>
2025-02-21 21:25:14 -0500

    Add functionality of hovering to blacken a box within the grid

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ function gridCreate(){
 
     const gridContainer = document.getElementById("grid-container");
     
+    for (let i = 1; i <= 256; i++){
+        const newGridItem = document.createElement("div");
+        newGridItem.classList.add("grid-item");
+        gridContainer.appendChild(newGridItem);
+    }
+
+    const gridItems = document.getElementsByClassName("grid-item");
     
-    for (let i = 1; i <= 16; i++){
-        for (let i = 1; i <= 16; i++){
-            const newGridItem = document.createElement("div");
-            newGridItem.classList.add("grid-item");
-            gridContainer.appendChild(newGridItem);
-        }
-        
-        /*const lineBreak = document.createElement('br');
-        gridContainer.appendChild(lineBreak);*/
+    for (let i = 0; i < gridItems.length; i++){
+        gridItems[i].addEventListener("mouseover", function() {
+                gridItems[i].style.backgroundColor = 'black';
+        });
     }
 
 }
